9fa241aaefa6dbc3c83bcf0e0ab68dd846197986
shenlu <shenlu@VPN-158-147.vpn.usf.edu>
2020-05-02 20:43:17 -0400

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Line Chart by Venkata Karthik Thota
-Animated D3.js Line Chart of Energy Per Capita (2000 - 2010) of BRICS + USA.
--- transpose.py
+++
@@ -1,5 +0,0 @@
-import csv
-from itertools import izip
-a = izip(*csv.reader(open("EPC_2000_2010.csv", "rb")))
-
-csv.writer(open("output.csv", "wb")).writerows(a)
