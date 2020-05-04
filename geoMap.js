1da7bd53f1bdae020fb33666f62367b79b27aef9
shenlu <shenlu@shenlus-MacBook-Air.local>
2020-05-03 21:20:33 -0400

    update

--- geoMap.js
+++ geoMap.js
@@ -1,24 +1,77 @@
-d3.json('us-states.json', function(data){
+var width = 960,
+    height = 500;
 
+var projection = d3.geo.mercator()
+    .center([0, 5 ])
+    .scale(200)
+    .rotate([-180,0]);
 
-var group = canvas.selectAll('g')
-    .data(data.features)
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var g = svg.append("g");
+
+// load and display the World
+d3.json("world-110m2.json", function(error, topology) {
+
+// load and display the cities
+d3.csv("cities.csv", function(error, data) {
+    g.selectAll("circle")
+       .data(data)
+       .enter()
+     .append("a")
+                  .attr("xlink:href", function(d) {
+                      return "https://www.google.com/search?q="+d.city;}
+                  )
+     .append("circle")
+       .attr("cx", function(d) {
+               return projection([d.lon, d.lat])[0];
+       })
+       .attr("cy", function(d) {
+               return projection([d.lon, d.lat])[1];
+       })
+       .attr("r", 5)
+       .style("fill", "red");
+       
+    g.selectAll("text")
+       .data(data)
+       .enter()
+     .append("text") // append text
+       .attr("x", function(d) {
+               return projection([d.lon, d.lat])[0];
+       })
+       .attr("y", function(d) {
+               return projection([d.lon, d.lat])[1];
+       })
+       .attr("dy", -7) // set y position of bottom of text
+      .style("fill", "black") // fill the text with the colour black
+      .attr("text-anchor", "middle") // set anchor y justification
+      .text(function(d) {return d.city;}); // define the text to display
+
+});
+
+g.selectAll("path")
+      .data(topojson.object(topology, topology.objects.countries)
+          .geometries)
     .enter()
-    .append('g')
+      .append("path")
+      .attr("d", path)
+});
 
-var projection =  d3.geo.mercator().scale([2400]).translate([-900,2650]);
-var path = d3.geo.path().projection(projection);
+// zoom and pan
+var zoom = d3.behavior.zoom()
+    .on("zoom",function() {
+        g.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g.selectAll("path")  
+            .attr("d", path.projection(projection)); 
 
-var areas = group.append('path')
-         .attr('d',path)
-         .attr('class','area')
-         .attr('fill','steelblue');
+  });
 
-
-group.append('text')
-     .attr('text',function(d){return d.properties.NAME_1;})
-     .attr('x', function(d){ return path.centroid(d)[0];})
-     .attr('y', function(d){ return path.centroid(d)[1];})
-     .style("font-size","14px");
-
- };
+svg.call(zoom)
