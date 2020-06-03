90d771180e675d17676021ea6d083dc79c049793
shen lu <shenlu@VPN-158-94.vpn.usf.edu>
2020-06-03 13:29:36 -0400

    barchar

--- 7420ae0e4ed77fc4@728.js
+++ 7420ae0e4ed77fc4@728.js
@@ -11,67 +11,7 @@ md`# Learn D3: By Example
 
 One of the best ways to start with D3 is to [browse the gallery](/@d3/gallery).`
 )});
-  main.variable(observer()).define(["md"], function(md){return(
-md`If you’re lucky (and with many to choose from, your odds aren’t bad), you may find an example you can repurpose as-is. This can save a lot of effort over learning how to build from scratch. For example, to construct a treemap, you *could* consult the [d3-hierarchy API reference](https://github.com/d3/d3-hierarchy/blob/master/README.md), but much faster would be to take the [treemap example](https://observablehq.com/@d3/treemap) and substitute your data.`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
-md`This may feel like cheating, but it’s okay! 👍 Examples are not merely reusable templates but tools for learning, hinting at subjects for study. “Breaking” an example by tinkering—changing stuff and seeing what happens—helps you achieve understanding faster than passive reading.
 
-Plus, it’s fun. 😺`
-)});
-  main.variable(observer()).define(["md","FileAttachment"], async function(md,FileAttachment){return(
-md`<figure>
-  <img src="${await FileAttachment("changing-stuff@1.jpg").url()}" style="width: 274px; height: 360px;">
-  <figcaption>A legitimate learning strategy. Image: [DEV Community](https://twitter.com/thepracticaldev)</figcaption>
-</figure>`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
-md`Not by coincidence, Observable is designed to help you tinker. Visit any notebook, edit a cell, and see what happens! In many cases all it takes to substitute your data is clicking to replace a file. If you like the result, save your work by forking.`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
-md`You can also import examples and tinker with them without diving into the code. This is undoubtedly the fastest way to start with D3, and it’ll grow more powerful as you learn.`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
-md`Let’s see how.
-
-Say I hand you a set of numbers. What might you tell me about this data?`
-)});
-  main.variable(observer("values")).define("values", ["FileAttachment"], function(FileAttachment){return(
-FileAttachment("values@1.json").json()
-)});
-  main.variable(observer()).define(["md"], function(md){return(
-md`You could inspect a few values by hand, but this won’t reveal much. There’s no way to know if the handful of values you inspect is representative of the overall distribution. What if you computed a few [summary statistics](/@d3/d3-mean-d3-median-and-friends), such as the min, median and max, by [requiring](/@observablehq/introduction-to-require) D3 (version 5)?`
-)});
-  main.variable(observer("d3")).define("d3", ["require"], function(require){return(
-require("d3@5")
-)});
-  main.variable(observer()).define(["d3","values"], function(d3,values){return(
-[d3.min(values), d3.median(values), d3.max(values)]
-)});
-  main.variable(observer()).define(["md"], function(md){return(
-md`Well… that’s something. But we need something richer than a single number to get a sense of the distribution.
-
-We need a visualization. Specifically, a histogram. So let’s [import](/@observablehq/introduction-to-imports) the [histogram example](/@d3/histogram) and use the *with* clause of the import (a feature unique to Observable JavaScript) to substitute our *values* array of numbers as the *data*.`
-)});
-  const child1 = runtime.module(define1).derive([{name: "values", alias: "data"}], main);
-  main.import("chart", "chart1", child1);
-  main.variable(observer()).define(["chart1"], function(chart1){return(
-chart1
-)});
-  main.variable(observer()).define(["md","tex"], function(md,tex){return(
-md`*Et voilà!* Now we see: the data roughly forms a normal distribution centered on zero. (I created these values using [d3.randomNormal](/@d3/d3-random) with an expected value ${tex`\mu = 0`} and standard deviation ${tex`\sigma = 1`}.)`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
-md`But we’re not limited to substituting data when we import. We can override any cell, say to customize the appearance of the *x*- or *y*-axis. Or if we’d prefer a slightly shorter chart, we can redefine the height.`
-)});
-  main.variable(observer("height")).define("height", function(){return(
-200
-)});
-  const child2 = runtime.module(define1).derive([{name: "values", alias: "data"},"height"], main);
-  main.import("chart", "chart2", child2);
-  main.variable(observer()).define(["chart2"], function(chart2){return(
-chart2
-)});
   main.variable(observer()).define(["md"], function(md){return(
 md`Did you notice fewer ticks along the *y*-axis in this shorter chart? Magic! And if you think that’s cool, watch this histogram come alive as we inject *dynamic* data! Click play below or drag the slider.`
 )});
